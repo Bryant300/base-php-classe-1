73430816699225fb0888f51d7d48672fb6dab092
Bryan <bryanbn@outlook.be>
2026-03-27 09:15:37 +0100

    .

--- Stagiaire/Bryan/mvc-19/public/index.php
+++ Stagiaire/Bryan/mvc-19/public/index.php
@@ -7,7 +7,7 @@ Chargement une seul fois
 et qui arrête
 le script en cas d'échec
 */
-require_once "../config.php"
+require_once '../config.php';
 
 # Affichage du __DIR__
 echo 'Chemin vers le dossier public via __DIR__ : '.__DIR__;
